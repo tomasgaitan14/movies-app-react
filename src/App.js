15086ddf0b74e6c8fff7a42106af66108ae0567f
tomasgaitan14 <tomasagustingaitan@gmail.com>
2023-04-19 17:36:43 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import MoviesGrid from "./MoviesGrid";
 import styles from "./App.module.css"
-import 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css';
 
  function App() {
   return (
     <div>
       <header>
-      <h1 className={animate__animated}>An animated element</h1>
         <h1 className={styles.title}>Movies</h1>
       </header>
       <main>
